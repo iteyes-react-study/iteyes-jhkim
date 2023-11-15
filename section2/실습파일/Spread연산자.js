391522d8716b38a223eeecda2bccd8931907e395
김준현 <sonjs01@naver.com>
2023-11-15 18:04:26 +0900

    Spread연산자 실습파일 추가

---
+++ section2/실습파일/Spread연산자.js
@@ -0,0 +1,6 @@
+const noTopingCookies =["촉촉한쿠키", "안촉촉한쿠키"];
+const topingCookies = ["바나나쿠키","블루베리쿠키","딸기쿠키"];
+
+const allCookies = [...noTopingCookies,...topingCookies]; // 배열의 모든 쿠키 정보가 담김
+
+console.log(allCookies);
